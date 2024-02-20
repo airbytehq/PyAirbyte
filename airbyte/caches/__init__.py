6d3af61a889ad01e54b99befa94422aa36c22ed9
Aaron Steers <aj@airbyte.io>
2024-02-19 16:54:28 -0800

    clean up imports

--- airbyte/caches/__init__.py
+++ airbyte/caches/__init__.py
@@ -1,19 +1,18 @@
 """Base module for all caches."""
 from __future__ import annotations
 
-from airbyte.caches.base import SQLCacheInstanceBase
-from airbyte.caches.duckdb import DuckDBCacheInstance, DuckDBCache
+from airbyte.caches.base import SQLCacheConfigBase
+from airbyte.caches.duckdb import DuckDBCache
 from airbyte.caches.postgres import PostgresCache, PostgresCacheConfig
 from airbyte.caches.snowflake import SnowflakeCacheConfig, SnowflakeSQLCache
 
 
 # We export these classes for easy access: `airbyte.caches...`
 __all__ = [
-    "DuckDBCacheInstance",
     "DuckDBCache",
     "PostgresCache",
     "PostgresCacheConfig",
-    "SQLCacheInstanceBase",
+    "SQLCacheConfigBase",
     "SnowflakeCacheConfig",
     "SnowflakeSQLCache",
 ]
