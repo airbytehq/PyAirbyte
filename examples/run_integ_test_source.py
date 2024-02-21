8f7d00613efdc2e7eee83b8da5a104c6d8a40ccd
Aaron Steers <aj@airbyte.io>
2024-02-21 11:03:06 -0800

    fix script issues (working and passing lint)

--- examples/run_integ_test_source.py
+++ examples/run_integ_test_source.py
@@ -20,9 +20,10 @@ import airbyte as ab
 def get_integ_test_config(secret_name: str) -> dict[str, Any]:
     if "GCP_GSM_CREDENTIALS" not in os.environ:
         raise Exception(  # noqa: TRY002, TRY003
-            f"GCP_GSM_CREDENTIALS env variable not set, can't fetch secrets for '{connector_name}'. "
+            f"GCP_GSM_CREDENTIALS env var not set, can't fetch secrets for '{connector_name}'. "
             "Make sure they are set up as described: "
-            "https://github.com/airbytehq/airbyte/blob/master/airbyte-ci/connectors/ci_credentials/README.md#get-gsm-access"
+            "https://github.com/airbytehq/airbyte/blob/master/airbyte-ci/connectors/ci_credentials/"
+            "README.md#get-gsm-access"
         )
 
     secret_client = secretmanager.SecretManagerServiceClient.from_service_account_info(
@@ -35,33 +36,44 @@ def get_integ_test_config(secret_name: str) -> dict[str, Any]:
     )
 
 
-def main(connector_name: str, secret_name: str | None) -> None:
+def main(
+    connector_name: str,
+    secret_name: str | None,
+    streams: list[str] | None,
+) -> None:
     config = get_integ_test_config(secret_name)
     source = ab.get_source(
         connector_name,
         config=config,
         install_if_missing=True,
     )
-    source.select_all_streams()
+    if streams:
+        source.select_streams(streams)
+    else:
+        source.select_all_streams()
     cache = ab.new_local_cache()
     try:
         read_result = source.read(cache=cache)
         print(
             f"Read from `{connector_name}` was successful. ",
-            f"Cache results were saved to: {cache.cache_dir}",
+            f"Cache results were saved to: {cache.config.cache_dir}",
             f"Streams list: {', '.join(read_result.streams.keys())}",
         )
-    except Exception as ex:
+    except Exception:
         print(
             f"Read from `{connector_name}` failed. ",
-            f"Cache files are located at: {cache.cache_dir}",
+            f"Cache files are located at: {cache.config.cache_dir}",
         )
-        sys.exit(1)
+        raise
 
 
 if __name__ == "__main__":
     # Get first arg from CLI
     connector_name = sys.argv[1]
+    streams_csv = sys.argv[2] if len(sys.argv) > 2 else None  # noqa: PLR2004
+    streams = None
+    if streams_csv:
+        streams = streams_csv.split(",")
     # TODO: We can optionally take a second arg to override the default secret name.
     secret_name = f"SECRET_{connector_name.upper()}__CREDS"
-    main(connector_name, secret_name)
+    main(connector_name, streams=streams, secret_name=secret_name)
