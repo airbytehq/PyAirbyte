71a4187f3d45d7f197387eaa23fb20c4f96681a4
Aaron Steers <aj@airbyte.io>
2024-02-23 08:39:00 -0800

    add test script

---
+++ examples/run_faker_to_motherduck.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+from __future__ import annotations
+
+import airbyte as ab
+from airbyte.caches import MotherDuckCache
+
+
+MOTHERDUCK_API_KEY = ab.get_secret("MOTHERDUCK_API_KEY")
+"""This is the API key for the MotherDuck service.
+
+It can be auto-detected in env vars and/or a .env file in the root of the project.
+
+If will be prompted (and masked during input) if not found in either location.
+"""
+
+
+source = ab.get_source(
+    "source-faker",
+    config={"count": 10000, "seed": 0, "parallelism": 1, "always_updated": False},
+    install_if_missing=True,
+)
+
+cache = MotherDuckCache(
+    database="airbyte_test",
+    schema_name="airbyte_test",
+    api_key=MOTHERDUCK_API_KEY,
+)
+
+source.check()
+
+source.select_all_streams()
+result = source.read(cache=cache)
+
+for name, records in result.streams.items():
+    print(f"Stream {name}: {len(records)} records")
