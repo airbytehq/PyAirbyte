8d05f80037b83a252c77ded348a8e2616fab9bf4
Aaron Steers <aj@airbyte.io>
2024-02-26 22:02:36 -0800

    add example script

---
+++ examples/run_langchain_github_test.py
@@ -0,0 +1,23 @@
+import airbyte as ab
+
+
+read_result = ab.get_source(
+    "source-github",
+    config={
+        "repositories": ["airbytehq/quickstarts"],
+        "credentials": {"personal_access_token": ab.get_secret("GITHUB_PERSONAL_ACCESS_TOKEN")},
+    },
+    streams=["issues"],
+).read()
+
+rendering = ab.documents.DocumentRenderer(
+    title_property="title",
+    cursor_property="updated_at",
+    body_properties=["body"],
+    metadata_properties=["state"],
+    primary_key_properties=["id"],
+    render_frontmatter=True,
+)
+
+for doc in rendering.render_documents(read_result["issues"]):
+    print(str(doc))
