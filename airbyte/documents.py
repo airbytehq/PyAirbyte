636c70f9364ec0fac19beff021baaa5205e7dcdd
Aaron Steers <aj@airbyte.io>
2024-03-04 21:09:06 -0800

    try with explicit pydantic Field() syntax

--- airbyte/documents.py
+++ airbyte/documents.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 if TYPE_CHECKING:
@@ -30,10 +30,10 @@ class Document(BaseModel):
     This class is duck-typed to be compatible with LangChain project's `Document` class.
     """
 
-    id: str | None = None
+    id: str | None = Field(default=None)
     content: str
     metadata: dict[str, Any]
-    last_modified: datetime.datetime | None = None
+    last_modified: datetime.datetime | None = Field(default=None)
 
     def __str__(self) -> str:
         return self.content
