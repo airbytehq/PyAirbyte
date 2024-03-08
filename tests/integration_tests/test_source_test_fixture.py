1a418cbbdd07077432f66ab064c1f5bad77bfd92
Aaron Steers <aj@airbyte.io>
2024-03-07 16:22:51 -0800

    fix patch ref

--- tests/integration_tests/test_source_test_fixture.py
+++ tests/integration_tests/test_source_test_fixture.py
@@ -87,7 +87,7 @@ def test_invalid_config():
 def test_ensure_installation_detection():
     """Assert that install isn't called, since the connector is already installed by the fixture."""
     with patch("airbyte._executor.VenvExecutor.install") as mock_venv_install, \
-         patch("airbyte.source.Source.install") as mock_source_install, \
+         patch("airbyte.sources.base.Source.install") as mock_source_install, \
          patch("airbyte._executor.VenvExecutor.ensure_installation") as mock_ensure_installed:
         source = ab.get_source(
             "source-test",
