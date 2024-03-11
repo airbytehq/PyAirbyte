9297169acb085621553b196d9d0c19838a714a22
Aaron Steers <aj@airbyte.io>
2024-03-11 13:11:56 -0700

    wip: add basic source capability to caches

--- airbyte/caches/base.py
+++ airbyte/caches/base.py
@@ -7,11 +7,15 @@ import abc
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, Optional, cast, final
 
+import ulid
 from pydantic import BaseModel, PrivateAttr
 
 from airbyte import exceptions as exc
 from airbyte.caches._catalog_manager import CatalogManager
+from airbyte.caches.util import get_default_cache
 from airbyte.datasets._sql import CachedDataset
+from airbyte.results import ReadResult
+from airbyte.strategies import WriteStrategy
 
 
 if TYPE_CHECKING:
@@ -46,6 +50,11 @@ class CacheBase(BaseModel):
 
     _sql_processor_class: type[SqlProcessorBase] = PrivateAttr()
     _sql_processor: Optional[SqlProcessorBase] = PrivateAttr(default=None)
+
+    @property
+    def name(self) -> str:
+        """Return the name of the cache."""
+        return self.__class__.__name__
 
     @final
     @property
@@ -123,3 +132,123 @@ class CacheBase(BaseModel):
 
     def __iter__(self) -> Generator[tuple[str, Any], None, None]:
         return ((name, dataset) for name, dataset in self.streams.items())
+
+    # DB-As-Source
+
+    def get_available_streams(self) -> list[str]:
+        """Get the available streams from the spec."""
+        return self.processor.get_tables_list()
+
+    def _log_sync_start(self, cache: CacheBase) -> None:
+        """TODO: Implement logging"""
+
+    def _write_table_to_files(
+        self,
+        table: str,
+        output_path: Path,
+    ) -> None:
+        """Write a table to files."""
+        self._write_table_to_files(
+            table=table,
+            output_path=output_path,
+        )
+
+    def read(
+        self,
+        cache: CacheBase | None = None,
+        *,
+        streams: str | list[str] | None = None,
+        write_strategy: str | WriteStrategy = WriteStrategy.AUTO,
+        force_full_refresh: bool = False,
+    ) -> ReadResult:
+        """Read from the connector and write to the cache.
+
+        Args:
+            cache: The cache to write to. If None, a default cache will be used.
+            write_strategy: The strategy to use when writing to the cache. If a string, it must be
+                one of "append", "upsert", "replace", or "auto". If a WriteStrategy, it must be one
+                of WriteStrategy.APPEND, WriteStrategy.UPSERT, WriteStrategy.REPLACE, or
+                WriteStrategy.AUTO.
+            streams: Optional if already set. A list of stream names to select for reading. If set
+                to "*", all streams will be selected.
+            force_full_refresh: If True, the source will operate in full refresh mode. Otherwise,
+                streams will be read in incremental mode if supported by the connector. This option
+                must be True when using the "replace" strategy.
+        """
+        if not streams:
+            raise exc.AirbyteLibInputError(
+                message="No streams selected.",
+                context={
+                    "available_streams": self.processor.get_tables_list(),
+                },
+            )
+
+        if isinstance(streams, str):
+            streams = [streams]
+
+        if write_strategy == WriteStrategy.REPLACE and not force_full_refresh:
+            raise exc.AirbyteLibInputError(
+                message="The replace strategy requires full refresh mode.",
+                context={
+                    "write_strategy": write_strategy,
+                    "force_full_refresh": force_full_refresh,
+                },
+            )
+        if cache is None:
+            cache = get_default_cache()
+
+        if isinstance(write_strategy, str):
+            try:
+                write_strategy = WriteStrategy(write_strategy)
+            except ValueError:
+                raise exc.AirbyteLibInputError(
+                    message="Invalid strategy",
+                    context={
+                        "write_strategy": write_strategy,
+                        "available_strategies": [s.value for s in WriteStrategy],
+                    },
+                ) from None
+
+        cache.processor.register_source(
+            source_name=self.name,
+            incoming_source_catalog=self.configured_catalog,
+            stream_names=set(streams),
+        )
+
+        state = (
+            self._get_state(
+                source_name=self.name,
+                streams=streams,
+            )
+            if not force_full_refresh
+            else None
+        )
+        self._log_sync_start(cache=cache)
+        job_files_root = Path(".") / "job_files" / ulid.ULID()
+        for stream_name in streams:
+            stream_files_path = job_files_root / stream_name
+            self._write_table_to_files(
+                table=self.processor.get_sql_table_name(stream_name),
+                output_path=stream_files_path,
+            )
+            temp_table_name: str = cache.processor._write_files_to_new_table(
+                files=(job_files_path / stream_name).files(),
+                source_name=self.name,
+                batch_id=ulid.ULID(),
+            )
+            final_table_name: str = cache.processor._get_final_table_name(
+                stream_name=stream_name,
+            )
+            self._write_temp_table_to_final_table(
+                stream_name=stream_name,
+                temp_table_name=temp_table_name,
+                final_table_name=final_table_name,
+                write_strategy=write_strategy,
+            )
+
+        self._log_sync_success(cache=cache)
+        return ReadResult(
+            processed_records=self._processed_records,
+            cache=cache,
+            processed_streams=[stream.stream.name for stream in self.configured_catalog.streams],
+        )
