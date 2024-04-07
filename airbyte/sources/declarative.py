47ec7a4af24d07312c1516705d72831229e42040
Aaron Steers <aj@airbyte.io>
2024-04-07 00:25:48 -0700

    skeleton of native declarative source support

---
+++ airbyte/sources/declarative.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+"""Support for declarative yaml source testing."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from source_declarative_manifest import run
+
+from airbyte._executor import Executor
+from airbyte.sources.base import Source
+
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+    from pathlib import Path
+
+
+class DeclarativeExecutor(Executor):
+    """An executor for declarative sources."""
+
+    def __init__(
+        self,
+        manifest: str | dict | Path,
+    ) -> None:
+        """Initialize a declarative executor."""
+        self.manifest = manifest
+
+    def execute(self, args: list[str]) -> Iterator[str]:
+        """Execute the declarative source."""
+        return run(args)
+
+    def ensure_installation(self, *, auto_fix: bool = True) -> None:
+        """No-op. The declarative source is included with PyAirbyte."""
+        _ = auto_fix
+        pass
+
+    def install(self) -> None:
+        """No-op. The declarative source is included with PyAirbyte."""
+        pass
+
+    def uninstall(self) -> None:
+        """No-op. The declarative source is included with PyAirbyte."""
+        pass
+
+
+class DeclarativeSource(Source):
+    """A declarative source using Airbyte's Yaml low-code/no-code framework."""
+
+    executor_class = DeclarativeExecutor
+
+    def __init__(
+        self,
+        manifest: str | dict | Path,
+    ) -> None:
+        """Initialize a declarative source."""
+        self.manifest = manifest
+        super().__init__(
+            name="Declarative",  # TODO: Get name from manifest
+            config={  # TODO: Put 'real' config here
+                "manifest": manifest,
+            },
+            executor=DeclarativeExecutor(manifest),
+        )
