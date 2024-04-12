c8945667c7b8d907e387b5302279760f275c6b79
Aaron Steers <aj@airbyte.io>
2024-04-11 18:05:20 -0700

    fix missing import

--- airbyte/_util/api_imports.py
+++ airbyte/_util/api_imports.py
@@ -4,6 +4,14 @@
 Any classes that are imported from the Airbyte API should be imported here.
 This allows for easy access to these classes in other modules, especially
 for type hinting purposes.
+
+Design Guidelines:
+- No modules except `api_util` and `api_imports` should import from `airbyte_api`.
+- If a module needs to import from `airbyte_api`, it should import from `api_imports` (this module)
+  instead.
+- This module is divided into two sections: internal-use classes and public-use classes.
+- Public-use classes should be carefully reviewed to ensure that they are necessary for public use
+  and that we are willing to support them as part of PyAirbyte.
 """
 # Ignore import sorting in this file. Manual grouping is more important.
 # ruff: noqa: I001
@@ -15,6 +23,7 @@ from __future__ import annotations
 # These classes are used internally to cache API responses.
 from airbyte_api.models import (
     ConnectionResponse,
+    DestinationResponse,
     JobResponse,
 )
 
@@ -27,6 +36,7 @@ from airbyte_api.models import JobStatusEnum  # Alias not needed
 
 __all__: list[str] = [
     "ConnectionResponse",
+    "DestinationResponse",
     "JobResponse",
     "JobStatusEnum",
 ]
