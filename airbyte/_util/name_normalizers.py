f67bd6803d89bbd0b123ed85f3008e929002dbef
Aaron Steers <aj@airbyte.io>
2024-05-15 16:53:34 -0700

    handle fully-numeric names

--- airbyte/_util/name_normalizers.py
+++ airbyte/_util/name_normalizers.py
@@ -56,6 +56,7 @@ class LowerCaseNormalizer(NameNormalizerBase):
         - Leading and trailing underscores are removed.
         - "%" is replaced with "pct".
         - "#" is replaced with "num".
+        - "+" is replaced with "_plus_".
 
         Examples:
         - "Hello World!" -> "hello_world"
@@ -64,10 +65,11 @@ class LowerCaseNormalizer(NameNormalizerBase):
         - "___Hello, World___" -> "hello_world"
         - "Average Sales (%)" -> "average_sales__pct"
         - "Average Sales (#)" -> "average_sales__num"
+        - "+1" -> "plus_1"
         """
         result = name
         # Replace "%" or "#" with "pct" or "num".
-        result = result.replace("%", "pct").replace("#", "num")
+        result = result.replace("%", "pct").replace("#", "num").replace("+", "_plus_")
 
         # Replace all non-alphanumeric characters with underscores.
         result = re.sub("[^A-Za-z0-9]", "_", result.lower())
@@ -77,6 +79,11 @@ class LowerCaseNormalizer(NameNormalizerBase):
 
         # Remove leading and trailing underscores.
         result = result.rstrip("_").lstrip("_")
+
+        # Check if name starts with a number and prepend "c" if it does.
+        if result and result[0].isdigit():
+            # Most databases do not allow identifiers to start with a number.
+            result = f"c{result}"
 
         if not result:
             raise exc.PyAirbyteNameNormalizationError(
