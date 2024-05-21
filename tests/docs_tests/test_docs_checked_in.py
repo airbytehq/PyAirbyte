866f6e1187743314f6c348564ec0bce601df80b1
Aaron Steers <aj@airbyte.io>
2024-05-21 14:52:14 -0700

    ignore warnings during docs generation

--- tests/docs_tests/test_docs_checked_in.py
+++ tests/docs_tests/test_docs_checked_in.py
@@ -3,14 +3,17 @@
 import os
 
 import docs.generate as generate
+import pytest
 
 
-def test_docs_checked_in():
+@pytest.mark.filterwarnings("ignore")
+def test_docs_generation():
     """
-    Docs need to be generated via `poetry run generate-docs` and checked in to the repo.
+    Docs need to be able to be generated via `poetry run generate-docs`.
 
-    This test runs the docs generation and compares the output with the checked in docs.
-    It will fail if there are any differences.
+    This test runs the docs generation and ensures that it can complete successfully.
+
+    Generation often produces warnings that are not relevant, so we suppress warnings in this test.
     """
 
     generate.run()
