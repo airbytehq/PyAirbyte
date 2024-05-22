3d0dce4b12f0ae4f58a13429174468d6f8cbee35
Aaron Steers <aj@airbyte.io>
2024-05-22 09:57:42 -0700

    refactor: move common code from Source class to ConnectorBase

--- airbyte/sources/base.py
+++ airbyte/sources/base.py
@@ -1,16 +1,11 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 from __future__ import annotations
 
-import json
 import warnings
-from pathlib import Path
 from typing import TYPE_CHECKING, Any, cast
 
-import jsonschema
 import pendulum
-import yaml
 from rich import print
-from rich.syntax import Syntax
 from typing_extensions import Literal
 
 from airbyte_protocol.models import (
@@ -18,11 +13,8 @@ from airbyte_protocol.models import (
     AirbyteMessage,
     ConfiguredAirbyteCatalog,
     ConfiguredAirbyteStream,
-    ConnectorSpecification,
     DestinationSyncMode,
-    Status,
     SyncMode,
-    TraceType,
     Type,
 )
 
@@ -31,13 +23,12 @@ from airbyte._future_cdk.catalog_providers import CatalogProvider
 from airbyte._util.telemetry import (
     EventState,
     EventType,
-    log_config_validation_result,
-    log_source_check_result,
     send_telemetry,
 )
 from airbyte._util.temp_files import as_temp_files
 from airbyte.caches.util import get_default_cache
 from airbyte.datasets._lazy import LazyDataset
+from airbyte.destinations.base import ConnectorBase
 from airbyte.progress import progress
 from airbyte.records import StreamRecord
 from airbyte.results import ReadResult
@@ -47,17 +38,22 @@ from airbyte.warnings import PyAirbyteDataLossWarning
 
 if TYPE_CHECKING:
     from collections.abc import Generator, Iterable, Iterator
+    from pathlib import Path
 
     from airbyte_protocol.models.airbyte_protocol import AirbyteStream
 
     from airbyte._executor import Executor
     from airbyte._future_cdk.state_providers import StateProviderBase
+    from airbyte._future_cdk.state_writers import StateWriterBase
+    from airbyte._processors.file.base import FileWriterBase
     from airbyte.caches import CacheBase
     from airbyte.documents import Document
 
 
-class Source:  # noqa: PLR0904  # Ignore max publish methods
+class Source(ConnectorBase):
     """A class representing a source that can be called."""
+
+    connector_type: Literal["source"] = "source"
 
     def __init__(
         self,
@@ -72,16 +68,15 @@ class Source:  # noqa: PLR0904  # Ignore max publish methods
 
         If config is provided, it will be validated against the spec if validate is True.
         """
-        self.executor = executor
-        self.name = name
+        super().__init__(
+            executor=executor,
+            name=name,
+            config=config,
+            validate=validate,
+        )
         self._processed_records = 0
-        self._config_dict: dict[str, Any] | None = None
-        self._last_log_messages: list[str] = []
         self._discovered_catalog: AirbyteCatalog | None = None
-        self._spec: ConnectorSpecification | None = None
         self._selected_stream_names: list[str] = []
-        if config is not None:
-            self.set_config(config, validate=validate)
         if streams is not None:
             self.select_streams(streams)
 
@@ -140,36 +135,6 @@ class Source:  # noqa: PLR0904  # Ignore max publish methods
         """
         return self._selected_stream_names
 
-    def set_config(
-        self,
-        config: dict[str, Any],
-        *,
-        validate: bool = True,
-    ) -> None:
-        """Set the config for the connector.
-
-        If validate is True, raise an exception if the config fails validation.
-
-        If validate is False, validation will be deferred until check() or validate_config()
-        is called.
-        """
-        if validate:
-            self.validate_config(config)
-
-        self._config_dict = config
-
-    def get_config(self) -> dict[str, Any]:
-        """Get the config for the connector."""
-        return self._config
-
-    @property
-    def _config(self) -> dict[str, Any]:
-        if self._config_dict is None:
-            raise exc.AirbyteConnectorConfigurationMissingError(
-                guidance="Provide via get_source() or set_config()"
-            )
-        return self._config_dict
-
     def _discover(self) -> AirbyteCatalog:
         """Call discover on the connector.
 
@@ -187,128 +152,9 @@ class Source:  # noqa: PLR0904  # Ignore max publish methods
                 log_text=self._last_log_messages,
             )
 
-    def validate_config(self, config: dict[str, Any] | None = None) -> None:
-        """Validate the config against the spec.
-
-        If config is not provided, the already-set config will be validated.
-        """
-        spec = self._get_spec(force_refresh=False)
-        config = self._config if config is None else config
-        try:
-            jsonschema.validate(config, spec.connectionSpecification)
-            log_config_validation_result(
-                name=self.name,
-                state=EventState.SUCCEEDED,
-            )
-        except jsonschema.ValidationError as ex:
-            validation_ex = exc.AirbyteConnectorValidationFailedError(
-                message="The provided config is not valid.",
-                context={
-                    "error_message": ex.message,
-                    "error_path": ex.path,
-                    "error_instance": ex.instance,
-                    "error_schema": ex.schema,
-                },
-            )
-            log_config_validation_result(
-                name=self.name,
-                state=EventState.FAILED,
-                exception=validation_ex,
-            )
-            raise validation_ex from ex
-
     def get_available_streams(self) -> list[str]:
         """Get the available streams from the spec."""
         return [s.name for s in self.discovered_catalog.streams]
-
-    def _get_spec(self, *, force_refresh: bool = False) -> ConnectorSpecification:
-        """Call spec on the connector.
-
-        This involves the following steps:
-        * execute the connector with spec
-        * Listen to the messages and return the first AirbyteCatalog that comes along.
-        * Make sure the subprocess is killed when the function returns.
-        """
-        if force_refresh or self._spec is None:
-            for msg in self._execute(["spec"]):
-                if msg.type == Type.SPEC and msg.spec:
-                    self._spec = msg.spec
-                    break
-
-        if self._spec:
-            return self._spec
-
-        raise exc.AirbyteConnectorMissingSpecError(
-            log_text=self._last_log_messages,
-        )
-
-    @property
-    def config_spec(self) -> dict[str, Any]:
-        """Generate a configuration spec for this connector, as a JSON Schema definition.
-
-        This function generates a JSON Schema dictionary with configuration specs for the
-        current connector, as a dictionary.
-
-        Returns:
-            dict: The JSON Schema configuration spec as a dictionary.
-        """
-        return self._get_spec(force_refresh=True).connectionSpecification
-
-    def print_config_spec(
-        self,
-        format: Literal["yaml", "json"] = "yaml",  # noqa: A002
-        *,
-        output_file: Path | str | None = None,
-    ) -> None:
-        """Print the configuration spec for this connector.
-
-        Args:
-        - format: The format to print the spec in. Must be "yaml" or "json".
-        - output_file: Optional. If set, the spec will be written to the given file path. Otherwise,
-          it will be printed to the console.
-        """
-        if format not in {"yaml", "json"}:
-            raise exc.PyAirbyteInputError(
-                message="Invalid format. Expected 'yaml' or 'json'",
-                input_value=format,
-            )
-        if isinstance(output_file, str):
-            output_file = Path(output_file)
-
-        if format == "yaml":
-            content = yaml.dump(self.config_spec, indent=2)
-        elif format == "json":
-            content = json.dumps(self.config_spec, indent=2)
-
-        if output_file:
-            output_file.write_text(content)
-            return
-
-        syntax_highlighted = Syntax(content, format)
-        print(syntax_highlighted)
-
-    @property
-    def _yaml_spec(self) -> str:
-        """Get the spec as a yaml string.
-
-        For now, the primary use case is for writing and debugging a valid config for a source.
-
-        This is private for now because we probably want better polish before exposing this
-        as a stable interface. This will also get easier when we have docs links with this info
-        for each connector.
-        """
-        spec_obj: ConnectorSpecification = self._get_spec()
-        spec_dict = spec_obj.dict(exclude_unset=True)
-        # convert to a yaml string
-        return yaml.dump(spec_dict)
-
-    @property
-    def docs_url(self) -> str:
-        """Get the URL to the connector's documentation."""
-        # TODO: Replace with docs URL from metadata when available
-        return "https://docs.airbyte.com/integrations/sources/" + self.name.lower().replace(
-            "source-", ""
-        )
 
     @property
     def discovered_catalog(self) -> AirbyteCatalog:
@@ -338,6 +184,24 @@ class Source:  # noqa: PLR0904  # Ignore max publish methods
 
         # Filter for selected streams if set, otherwise use all available streams:
         streams_filter: list[str] = self._selected_stream_names or self.get_available_streams()
+        return self.get_configured_catalog(streams=streams_filter)
+
+    def get_configured_catalog(
+        self,
+        streams: Literal["*"] | list[str] | None = None,
+    ) -> ConfiguredAirbyteCatalog:
+        selected_streams: list[str] = []
+        if streams is None:
+            selected_streams = self._selected_stream_names or self.get_available_streams()
+        elif streams == "*":
+            selected_streams = self.get_available_streams()
+        elif isinstance(streams, list):
+            selected_streams = streams
+        else:
+            raise exc.PyAirbyteInputError(
+                message="Invalid streams argument.",
+                input_value=streams,
+            )
 
         return ConfiguredAirbyteCatalog(
             streams=[
@@ -350,7 +214,7 @@ class Source:  # noqa: PLR0904  # Ignore max publish methods
                     sync_mode=SyncMode.incremental,
                 )
                 for stream in self.discovered_catalog.streams
-                if stream.name in streams_filter
+                if stream.name in selected_streams
             ],
         )
 
@@ -436,14 +300,6 @@ class Source:  # noqa: PLR0904  # Ignore max publish methods
             stream_metadata=configured_stream,
         )
 
-    @property
-    def connector_version(self) -> str | None:
-        """Return the version of the connector as reported by the executor.
-
-        Returns None if the version cannot be determined.
-        """
-        return self.executor.get_installed_version()
-
     def get_documents(
         self,
         stream: str,
@@ -467,57 +323,6 @@ class Source:  # noqa: PLR0904  # Ignore max publish methods
             metadata_properties=metadata_properties,
             render_metadata=render_metadata,
         )
-
-    def check(self) -> None:
-        """Call check on the connector.
-
-        This involves the following steps:
-        * Write the config to a temporary file
-        * execute the connector with check --config <config_file>
-        * Listen to the messages and return the first AirbyteCatalog that comes along.
-        * Make sure the subprocess is killed when the function returns.
-        """
-        with as_temp_files([self._config]) as [config_file]:
-            try:
-                for msg in self._execute(["check", "--config", config_file]):
-                    if msg.type == Type.CONNECTION_STATUS and msg.connectionStatus:
-                        if msg.connectionStatus.status != Status.FAILED:
-                            print(f"Connection check succeeded for `{self.name}`.")
-                            log_source_check_result(
-                                name=self.name,
-                                state=EventState.SUCCEEDED,
-                            )
-                            return
-
-                        log_source_check_result(
-                            name=self.name,
-                            state=EventState.FAILED,
-                        )
-                        raise exc.AirbyteConnectorCheckFailedError(
-                            help_url=self.docs_url,
-                            context={
-                                "failure_reason": msg.connectionStatus.message,
-                            },
-                        )
-                raise exc.AirbyteConnectorCheckFailedError(log_text=self._last_log_messages)
-            except exc.AirbyteConnectorReadError as ex:
-                raise exc.AirbyteConnectorCheckFailedError(
-                    message="The connector failed to check the connection.",
-                    log_text=ex.log_text,
-                ) from ex
-
-    def install(self) -> None:
-        """Install the connector if it is not yet installed."""
-        self.executor.install()
-        print("For configuration instructions, see: \n" f"{self.docs_url}#reference\n")
-
-    def uninstall(self) -> None:
-        """Uninstall the connector if it is installed.
-
-        This only works if the use_local_install flag wasn't used and installation is managed by
-        PyAirbyte.
-        """
-        self.executor.uninstall()
 
     def _read_with_catalog(
         self,
@@ -559,11 +364,7 @@ class Source:  # noqa: PLR0904  # Ignore max publish methods
                 )
             )
 
-    def _add_to_logs(self, message: str) -> None:
-        self._last_log_messages.append(message)
-        self._last_log_messages = self._last_log_messages[-10:]
-
-    def _execute(self, args: list[str]) -> Iterator[AirbyteMessage]:
+    def _execute(self, args: list[str], stdin: None = None) -> Iterator[AirbyteMessage]:
         """Execute the connector with the given arguments.
 
         This involves the following steps:
@@ -572,27 +373,12 @@ class Source:  # noqa: PLR0904  # Ignore max publish methods
         * Read the output line by line of the subprocess and serialize them AirbyteMessage objects.
           Drop if not valid.
         """
-        # Fail early if the connector is not installed.
-        self.executor.ensure_installation(auto_fix=False)
+        _ = stdin  # Unused, but kept for compatibility with the base class
+        for message in super()._execute(args):
+            if message.type is Type.RECORD:
+                self._processed_records += 1
 
-        try:
-            self._last_log_messages = []
-            for line in self.executor.execute(args):
-                try:
-                    message = AirbyteMessage.parse_raw(line)
-                    if message.type is Type.RECORD:
-                        self._processed_records += 1
-                    if message.type == Type.LOG:
-                        self._add_to_logs(message.log.message)
-                    if message.type == Type.TRACE and message.trace.type == TraceType.ERROR:
-                        self._add_to_logs(message.trace.error.message)
-                    yield message
-                except Exception:
-                    self._add_to_logs(line)
-        except Exception as e:
-            raise exc.AirbyteConnectorReadError(
-                log_text=self._last_log_messages,
-            ) from e
+            yield message
 
     def _tally_records(
         self,
@@ -651,6 +437,31 @@ class Source:  # noqa: PLR0904  # Ignore max publish methods
             exception=exception,
             event_type=EventType.SYNC,
         )
+
+    def read_to_files(
+        self,
+        file_writer: FileWriterBase,
+        *,
+        streams: str | list[str] | None = None,
+        state_provider: StateProviderBase | None = None,
+        state_writer: StateWriterBase | None = None,
+        skip_validation: bool = False,
+    ) -> dict[str, list[Path]]:
+        """Read from the connector and write to the file, returning a dictionary of file paths."""
+        if not skip_validation:
+            self.validate_config()
+            self.check()
+
+        if streams:
+            self.select_streams(streams)
+
+        if not self._selected_stream_names:
+            raise exc.PyAirbyteNoStreamsSelectedError(
+                connector_name=self.name,
+                available_streams=self.get_available_streams(),
+            )
+
+        return file_writer._completed_batches
 
     def read(
         self,
