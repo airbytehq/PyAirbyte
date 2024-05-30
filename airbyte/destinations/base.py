0ad2af8b6daa463d94a216bde81621872ad52d50
Aaron Steers <aj@airbyte.io>
2024-05-30 10:39:51 -0700

    slight increment (wip)

--- airbyte/destinations/base.py
+++ airbyte/destinations/base.py
@@ -3,13 +3,15 @@
 
 from __future__ import annotations
 
+from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
 from typing_extensions import Literal
 
-from airbyte_protocol.models.airbyte_protocol import AirbyteMessage
+from airbyte_protocol.models.airbyte_protocol import AirbyteMessage, ConfiguredAirbyteCatalog
 
 from airbyte._connector_base import ConnectorBase
+from airbyte._future_cdk.record_processor import RecordProcessorBase
 from airbyte._util.temp_files import as_temp_files
 from airbyte.strategies import WriteStrategy
 
@@ -21,6 +23,10 @@ if TYPE_CHECKING:
     from airbyte._future_cdk import state_providers
     from airbyte._future_cdk.state_writers import StateWriterBase
     from airbyte.sources.base import Source
+
+
+class DestinationProcessor(RecordProcessorBase):
+    """Processor for Destinations."""
 
 
 class Destination(ConnectorBase):
@@ -47,7 +53,7 @@ class Destination(ConnectorBase):
             validate=validate,
         )
 
-    def _write_from_source(
+    def _write(
         self,
         source: Source,
         *,
@@ -59,19 +65,31 @@ class Destination(ConnectorBase):
     ) -> None:
         """Write records to the destination."""
         source.read()
-        configured_catalog_dict = source.get_configured_catalog(streams=streams).json()
+        configured_catalog: ConfiguredAirbyteCatalog = source.get_configured_catalog(
+            streams=streams
+        )
 
         if not skip_validation:
             source.validate_config()
             self.validate_config()
 
         source_msg_iterator: Iterator[AirbyteMessage] = source._read_with_catalog(  # noqa: SLF001
-            catalog=configured_catalog_dict,
+            catalog=configured_catalog,
             state=state_provider,
         )
         str_iterator: Generator[str, None, None] = (str(msg) for msg in source_msg_iterator)
         # Create an IO buffer to store a stringified version of the iterator output
 
+    def _write_stream(
+        self,
+        stream_name: str,
+        data_files: list[Path],
+        *,
+        configured_catalog: ConfiguredAirbyteCatalog,
+        write_strategy: str | WriteStrategy = WriteStrategy.AUTO,
+        state_writer: StateWriterBase,
+    ) -> None:
+        """Write records to the destination."""
         with as_temp_files(
             [
                 self._config,
