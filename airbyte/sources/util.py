01b8864b40ccbb731bb9aa1f2a67c90b7e573c97
tqtensor <victortang212@gmail.com>
2024-06-04 06:37:16 +0700

    chore: add noqa for PLR0913 temporarily

--- airbyte/sources/util.py
+++ airbyte/sources/util.py
@@ -45,7 +45,7 @@ def get_connector(
 
 # This non-public function includes the `docker_image` parameter, which is not exposed in the
 # public API. See the `experimental` module for more info.
-def _get_source(  # noqa: PLR0912  # Too many branches
+def _get_source(  # noqa: PLR0912, PLR0913  # Too many branches
     name: str,
     config: dict[str, Any] | None = None,
     *,
