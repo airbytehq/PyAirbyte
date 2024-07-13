658f745f230427043811bf1da7493c8db8e59f2b
Michihiro Nakamura <michihiro.nakamura@uzabase.com>
2024-07-14 06:21:39 +0900

    remove an incorrect comment

--- airbyte/_future_cdk/sql_processor.py
+++ airbyte/_future_cdk/sql_processor.py
@@ -303,7 +303,6 @@ class SqlProcessorBase(RecordProcessorBase):
     def _ensure_schema_exists(
         self,
     ) -> None:
-        """Return a new (unique) temporary table name."""
         dialect = self.get_sql_engine().dialect
         schema_name = dialect.normalize_name(self.sql_config.schema_name)
         if schema_name in self._get_schemas_list():
