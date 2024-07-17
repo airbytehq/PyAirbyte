56c252bcc9f9901c0d971e7d1d8f30592f8cc495
Aaron Steers <aj@airbyte.io>
2024-07-17 08:25:12 -0700

    chore: remove unneeded cortex declarations

--- airbyte/_processors/sql/__init__.py
+++ airbyte/_processors/sql/__init__.py
@@ -1,16 +1,2 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 """SQL processors."""
-
-from __future__ import annotations
-
-from airbyte._processors.sql.snowflakecortex import (
-    SnowflakeCortexSqlProcessor,
-    SnowflakeCortexTypeConverter,
-)
-
-
-__all__ = [
-    # Classes
-    "SnowflakeCortexSqlProcessor",
-    "SnowflakeCortexTypeConverter",
-]
