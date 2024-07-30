fb783e84e18384981a9bff94809cd644b7705471
Aaron Steers <aj@airbyte.io>
2024-07-30 11:59:54 -0700

    update docs homepage

--- airbyte/__init__.py
+++ airbyte/__init__.py
@@ -1,7 +1,20 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 """PyAirbyte brings Airbyte ELT to every Python developer.
 
-.. include:: ../README.md
+# PyAirbyte
+
+PyAirbyte brings the power of Airbyte to every Python developer. PyAirbyte provides a set of
+utilities to use Airbyte connectors in Python.
+
+[![PyPI version](https://badge.fury.io/py/airbyte.svg)](https://badge.fury.io/py/airbyte)
+[![PyPI - Downloads](https://img.shields.io/pypi/dm/airbyte)](https://pypi.org/project/airbyte/)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/airbyte)](https://pypi.org/project/airbyte/)
+<!-- [![PyPI - License](https://img.shields.io/pypi/l/airbyte)](https://pypi.org/project/airbyte/) -->
+[![PyPI - Wheel](https://img.shields.io/pypi/wheel/airbyte)](https://pypi.org/project/airbyte/)
+<!-- [![PyPI - Status](https://img.shields.io/pypi/status/airbyte)](https://pypi.org/project/airbyte/) -->
+[![PyPI - Implementation](https://img.shields.io/pypi/implementation/airbyte)](https://pypi.org/project/airbyte/)
+[![PyPI - Format](https://img.shields.io/pypi/format/airbyte)](https://pypi.org/project/airbyte/)
+[![Star on GitHub](https://img.shields.io/github/stars/airbytehq/pyairbyte.svg?style=social&label=★%20on%20GitHub)](https://github.com/airbytehq/pyairbyte)
 
 ## Reading Data
 
@@ -56,6 +69,54 @@ If you need to build your own secret manager, you can subclass the
 `airbyte.secrets.CustomSecretManager` class.
 
 For more information, see the `airbyte.secrets` module.
+
+## Frequently asked Questions
+
+**1. Does PyAirbyte replace Airbyte?**
+
+No. PyAirbyte is a Python library that allows you to use Airbyte connectors in Python but it does
+not have orchestration or scheduling capabilities, nor does is provide logging, alerting, or other
+features for managing data pipelines in production. Airbyte is a full-fledged data integration
+platform that provides connectors, orchestration, and scheduling capabilities.
+
+**2. What is the PyAirbyte cache? Is it a destination?**
+
+Yes and no. You can think of it as a built-in destination implementation, but we avoid the word
+"destination" in our docs to prevent confusion with our certified destinations list
+[here](https://docs.airbyte.com/integrations/destinations/).
+
+**3. Does PyAirbyte work with data orchestration frameworks like Airflow, Dagster, and Snowpark,
+etc.?**
+
+Yes, it should. Please give it a try and report any problems you see. Also, drop us a note if works
+for you!
+
+**4. Can I use PyAirbyte to develop or test when developing Airbyte sources?**
+
+Yes, you can. PyAirbyte makes it easy to test connectors in Python, and you can use it to develop
+new local connectors as well as existing already-published ones.
+
+**5. Can I develop traditional ETL pipelines with PyAirbyte?**
+
+Yes. Just pick the cache type matching the destination - like SnowflakeCache for landing data in
+Snowflake.
+
+**6. Can PyAirbyte import a connector from a local directory that has python project files, or does
+
+it have to be pip install**
+
+Yes, PyAirbyte can use any local install that has a CLI - and will automatically find connectors b
+name if they are on PATH.
+
+## Contributing
+
+To learn how you can contribute to PyAirbyte, please see our
+[PyAirbyte Contributors Guide](./CONTRIBUTING.md).
+
+## Changelog and Release Notes
+
+For a version history and list of all changes, please see our
+[GitHub Releases](https://github.com/airbytehq/PyAirbyte/releases) page.
 
 ## API Reference
 
