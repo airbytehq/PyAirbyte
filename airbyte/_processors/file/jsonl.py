f4d1f6a532aa64d930e38a7918e5a15e35f2ac49
Aaron Steers <aj@airbyte.io>
2024-08-02 14:14:44 -0700

    wip: move write operation to separate thread

--- airbyte/_processors/file/jsonl.py
+++ airbyte/_processors/file/jsonl.py
@@ -6,7 +6,9 @@ from __future__ import annotations
 import gzip
 import io
 import json
-from typing import IO, TYPE_CHECKING, cast
+import queue
+import threading
+from typing import IO, TYPE_CHECKING, Any, cast
 
 import orjson
 from overrides import overrides
@@ -28,22 +30,52 @@ class JsonlWriter(FileWriterBase):
     default_cache_file_suffix = ".jsonl.gz"
     prune_extra_fields = True
 
+    def __init__(self, *args: Any, **kwargs: Any) -> None:  # noqa: ANN401
+        self._current_file_writer: IO[str] | None = None
+        self._queue: queue.Queue[str] = queue.Queue()
+        self._stop_event = threading.Event()
+        self._thread = threading.Thread(target=self._write_worker)
+        self._thread.start()
+        super().__init__(*args, **kwargs)
+
     @overrides
     def _open_new_file(
         self,
         file_path: Path,
     ) -> IO[str]:
         """Open a new file for writing."""
+        # buffer_size = 1024 * 1024  # 1 MB
+
         gzip_file: gzip.GzipFile = gzip.open(
             file_path,
             mode="wb",
-            # encoding="utf-8",
         )
-        return io.TextIOWrapper(
-            io.BufferedWriter(cast(io.RawIOBase, gzip_file)),
+        self._current_file_writer = io.TextIOWrapper(
+            io.BufferedWriter(
+                cast(io.RawIOBase, gzip_file),
+                # buffer_size=buffer_size,
+            ),
             encoding="utf-8",
         )
+        return self._current_file_writer
 
+    def _write_worker(self) -> None:
+        while not self._stop_event.is_set() or not self._queue.empty():
+            try:
+                record = self._queue.get(timeout=1)
+                assert self._current_file_writer is not None
+                self._current_file_writer.write(record)
+                self._queue.task_done()
+            except queue.Empty:
+                continue
+
+    def close(self) -> None:
+        """Close the writer and wait for the worker thread to finish."""
+        self._queue.join()
+        self._stop_event.set()
+        self._thread.join()
+        assert self._current_file_writer is not None
+        self._current_file_writer.close()
 
     @overrides
     def _write_record_dict(
@@ -54,9 +86,13 @@ class JsonlWriter(FileWriterBase):
         # If the record is too nested, `orjson` will fail with error `TypeError: Recursion
         # limit reached`. If so, fall back to the slower `json.dumps`.
         try:
-            open_file_writer.write(orjson.dumps(record_dict).decode(encoding="utf-8") + "\n")
+            line = orjson.dumps(record_dict).decode(encoding="utf-8") + "\n"
         except TypeError:
             # Using isoformat method for datetime serialization
-            open_file_writer.write(
-                json.dumps(record_dict, default=lambda _: _.isoformat()) + "\n",
-            )
+            line = json.dumps(record_dict, default=lambda _: _.isoformat()) + "\n"
+        else:
+            # No exception occurred, so write the line to the file
+            if open_file_writer is not self._current_file_writer:
+                self._current_file_writer = open_file_writer
+
+            self._queue.put(line)
