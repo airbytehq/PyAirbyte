9b40875b9b8a0be381f792e2209f48decc2d88e9
Aaron Steers <aj@airbyte.io>
2024-10-14 22:13:50 -0700

    clean up cloud functionality

--- airbyte/cloud/workspaces.py
+++ airbyte/cloud/workspaces.py
@@ -11,25 +11,17 @@ from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 from airbyte import exceptions as exc
-from airbyte._util.api_util import (
-    CLOUD_API_ROOT,
-    create_connection,
-    create_destination,
-    create_source,
-    delete_connection,
-    delete_destination,
-    delete_source,
-    get_workspace,
-)
-from airbyte.cloud._destination_util import get_destination_config_from_cache
+from airbyte._util import api_util, text_util
 from airbyte.cloud.connections import CloudConnection
+from airbyte.cloud.connectors import CloudDestination, CloudSource
 from airbyte.cloud.sync_results import SyncResult
-from airbyte.sources.base import Source
 
 
 if TYPE_CHECKING:
-    from airbyte._util.api_imports import DestinationResponse
-    from airbyte.caches.base import CacheBase
+    from collections.abc import Callable
+
+    from airbyte.destinations.base import Destination
+    from airbyte.sources.base import Source
 
 
 @dataclass
@@ -42,7 +34,7 @@ class CloudWorkspace:
 
     workspace_id: str
     api_key: str
-    api_root: str = CLOUD_API_ROOT
+    api_root: str = api_util.CLOUD_API_ROOT
 
     @property
     def workspace_url(self) -> str | None:
@@ -58,194 +50,193 @@ class CloudWorkspace:
               serves primarily as a simple check to ensure that the workspace is reachable
               and credentials are correct.
         """
-        _ = get_workspace(
+        _ = api_util.get_workspace(
             api_root=self.api_root,
             api_key=self.api_key,
             workspace_id=self.workspace_id,
         )
         print(f"Successfully connected to workspace: {self.workspace_url}")
 
-    # Deploy and delete sources
+    # Deploy sources and destinations
 
-    # TODO: Make this a public API
-    # https://github.com/airbytehq/pyairbyte/issues/228
-    def _deploy_source(
+    def deploy_source(
         self,
+        name: str,
         source: Source,
-    ) -> str:
+        *,
+        unique: bool = True,
+        random_name_suffix: bool = False,
+    ) -> CloudSource:
         """Deploy a source to the workspace.
 
-        Returns the newly deployed source ID.
+        Returns the newly deployed source.
+
+        Args:
+            name: The name to use when deploying.
+            source: The source object to deploy.
+            unique: Whether to require a unique name. If `True`, duplicate names
+                are not allowed. Defaults to `True`.
+            random_name_suffix: Whether to append a random suffix to the name.
         """
         source_configuration = source.get_config().copy()
         source_configuration["sourceType"] = source.name.replace("source-", "")
 
-        deployed_source = create_source(
-            name=f"{source.name.replace('-', ' ').title()} (Deployed by PyAirbyte)",
+        if random_name_suffix:
+            name += f" (ID: {text_util.generate_random_suffix()})"
+
+        if unique:
+            existing = self.list_sources(name=name)
+            if existing:
+                raise exc.AirbyteDuplicateResourcesError(
+                    resource_type="destination",
+                    resource_name=name,
+                )
+
+        deployed_source = api_util.create_source(
+            name=name,
             api_root=self.api_root,
             api_key=self.api_key,
             workspace_id=self.workspace_id,
             config=source_configuration,
         )
+        return CloudSource(
+            workspace=self,
+            connector_id=deployed_source.source_id,
+        )
 
-        # Set the deployment Ids on the source object
-        source._deployed_api_root = self.api_root  # noqa: SLF001  # Accessing nn-public API
-        source._deployed_workspace_id = self.workspace_id  # noqa: SLF001  # Accessing nn-public API
-        source._deployed_source_id = deployed_source.source_id  # noqa: SLF001  # Accessing nn-public API
-
-        return deployed_source.source_id
-
-    def _permanently_delete_source(
+    def deploy_destination(
         self,
-        source: str | Source,
+        name: str,
+        destination: Destination,
+        *,
+        unique: bool = True,
+        random_name_suffix: bool = False,
+    ) -> CloudDestination:
+        """Deploy a destination to the workspace.
+
+        Returns the newly deployed destination ID.
+
+        Args:
+            name: The name to use when deploying.
+            destination: The destination object to deploy.
+            unique: Whether to require a unique name. If `True`, duplicate names
+                are not allowed. Defaults to `True`.
+            random_name_suffix: Whether to append a random suffix to the name.
+        """
+        destination_configuration = destination.get_config().copy()
+        destination_configuration["destinationType"] = destination.name.replace("destination-", "")
+
+        if random_name_suffix:
+            name += f" (ID: {text_util.generate_random_suffix()})"
+
+        if unique:
+            existing = self.list_destinations(name=name)
+            if existing:
+                raise exc.AirbyteDuplicateResourcesError(
+                    resource_type="destination",
+                    resource_name=name,
+                )
+
+        deployed_destination = api_util.create_destination(
+            name=name,
+            api_root=self.api_root,
+            api_key=self.api_key,
+            workspace_id=self.workspace_id,
+            config=destination_configuration,
+        )
+        return CloudDestination(
+            workspace=self,
+            connector_id=deployed_destination.destination_id,
+        )
+
+    def permanently_delete_source(
+        self,
+        source: str | CloudSource,
     ) -> None:
         """Delete a source from the workspace.
 
         You can pass either the source ID `str` or a deployed `Source` object.
         """
-        if not isinstance(source, str | Source):
-            raise ValueError(f"Invalid source type: {type(source)}")  # noqa: TRY004, TRY003
+        if not isinstance(source, str | CloudSource):
+            raise exc.PyAirbyteInputError(
+                message="Invalid source type.",
+                input_value=type(source).__name__,
+            )
 
-        if isinstance(source, Source):
-            if not source._deployed_source_id:  # noqa: SLF001
-                raise ValueError("Source has not been deployed.")  # noqa: TRY003
-
-            source_id = source._deployed_source_id  # noqa: SLF001
-
-        elif isinstance(source, str):
-            source_id = source
-
-        delete_source(
-            source_id=source_id,
+        api_util.delete_source(
+            source_id=source.connector_id if isinstance(source, CloudSource) else source,
             api_root=self.api_root,
             api_key=self.api_key,
         )
 
     # Deploy and delete destinations
 
-    # TODO: Make this a public API
-    # https://github.com/airbytehq/pyairbyte/issues/228
-    def _deploy_cache_as_destination(
+    def permanently_delete_destination(
         self,
-        cache: CacheBase,
-    ) -> str:
-        """Deploy a cache to the workspace as a new destination.
-
-        Returns the newly deployed destination ID.
-        """
-        cache_type_name = cache.__class__.__name__.replace("Cache", "")
-
-        deployed_destination: DestinationResponse = create_destination(
-            name=f"Destination {cache_type_name} (Deployed by PyAirbyte)",
-            api_root=self.api_root,
-            api_key=self.api_key,
-            workspace_id=self.workspace_id,
-            config=get_destination_config_from_cache(cache),
-        )
-
-        # Set the deployment Ids on the source object
-        cache._deployed_api_root = self.api_root  # noqa: SLF001  # Accessing nn-public API
-        cache._deployed_workspace_id = self.workspace_id  # noqa: SLF001  # Accessing nn-public API
-        cache._deployed_destination_id = deployed_destination.destination_id  # noqa: SLF001  # Accessing nn-public API
-
-        return deployed_destination.destination_id
-
-    def _permanently_delete_destination(
-        self,
-        *,
-        destination: str | None = None,
-        cache: CacheBase | None = None,
+        destination: str | CloudDestination,
     ) -> None:
         """Delete a deployed destination from the workspace.
 
         You can pass either the `Cache` class or the deployed destination ID as a `str`.
         """
-        if destination is None and cache is None:
-            raise ValueError("You must provide either a destination ID or a cache object.")  # noqa: TRY003
-        if destination is not None and cache is not None:
-            raise ValueError(  # noqa: TRY003
-                "You must provide either a destination ID or a cache object, not both."
+        if not isinstance(destination, str | CloudDestination):
+            raise exc.PyAirbyteInputError(
+                message="Invalid destination type.",
+                input_value=type(destination).__name__,
             )
 
-        if cache:
-            if not cache._deployed_destination_id:  # noqa: SLF001
-                raise ValueError("Cache has not been deployed.")  # noqa: TRY003
-
-            destination = cache._deployed_destination_id  # noqa: SLF001
-
-        if destination is None:
-            raise ValueError("No destination ID provided.")  # noqa: TRY003
-
-        delete_destination(
-            destination_id=destination,
+        api_util.delete_destination(
+            destination_id=(
+                destination if isinstance(destination, str) else destination.destination_id
+            ),
             api_root=self.api_root,
             api_key=self.api_key,
         )
 
     # Deploy and delete connections
 
-    # TODO: Make this a public API
-    # https://github.com/airbytehq/pyairbyte/issues/228
-    def _deploy_connection(
+    def deploy_connection(
         self,
-        source: Source | str,
-        cache: CacheBase | None = None,
-        destination: str | None = None,
+        connection_name: str,
+        *,
+        source: CloudSource | str,
+        destination: CloudDestination | str,
         table_prefix: str | None = None,
         selected_streams: list[str] | None = None,
     ) -> CloudConnection:
-        """Deploy a source and cache to the workspace as a new connection.
+        """Create a new connection between an already deployed source and destination.
 
-        Returns the newly deployed connection ID as a `str`.
+        Returns the newly deployed connection object.
 
         Args:
-            source (Source | str): The source to deploy. You can pass either an already deployed
-                source ID `str` or a PyAirbyte `Source` object. If you pass a `Source` object,
-                it will be deployed automatically.
-            cache (CacheBase, optional): The cache to deploy as a new destination. You can provide
-                `cache` or `destination`, but not both.
-            destination (str, optional): The destination ID to use. You can provide
-                `cache` or `destination`, but not both.
-            table_prefix (str, optional): The table prefix to use for the cache. If not provided,
-                the cache's table prefix will be used.
-            selected_streams (list[str], optional): The selected stream names to use for the
-                connection. If not provided, the source's selected streams will be used.
+            connection_name: The name of the connection.
+            source: The deployed source. You can pass a source ID or a CloudSource object.
+            destination: The deployed destination. You can pass a destination ID or a
+                CloudDestination object.
+            table_prefix: Optional. The table prefix to use when syncing to the destination.
+            selected_streams: The selected stream names to sync within the connection.
         """
+        if not selected_streams:
+            raise exc.PyAirbyteInputError(
+                guidance="You must provide `selected_streams` when creating a connection."
+            )
+
         # Resolve source ID
         source_id: str
-        if isinstance(source, Source):
-            selected_streams = selected_streams or source.get_selected_streams()
-            source_id = (
-                source._deployed_source_id  # noqa: SLF001  # Access to non-public API
-                or self._deploy_source(source)
-            )
-        else:
-            source_id = source
-            if not selected_streams:
-                raise exc.PyAirbyteInputError(
-                    guidance="You must provide `selected_streams` when deploying a source ID."
-                )
+        source_id = source if isinstance(source, str) else source.connector_id
 
-        # Resolve destination ID
-        destination_id: str
-        if destination:
-            destination_id = destination
-        elif cache:
-            table_prefix = table_prefix if table_prefix is not None else (cache.table_prefix or "")
-            if not cache._deployed_destination_id:  # noqa: SLF001
-                destination_id = self._deploy_cache_as_destination(cache)
-            else:
-                destination_id = cache._deployed_destination_id  # noqa: SLF001
-        else:
+        # destination is already deployed
+        destination_id = destination if isinstance(destination, str) else destination.connector_id
+        if not selected_streams:
             raise exc.PyAirbyteInputError(
-                guidance="You must provide either a destination ID or a cache object."
+                guidance=(
+                    "You must provide `selected_streams` when creating a connection "
+                    "from an existing destination."
+                )
             )
 
-        assert source_id is not None
-        assert destination_id is not None
-
-        deployed_connection = create_connection(
-            name="Connection (Deployed by PyAirbyte)",
+        deployed_connection = api_util.create_connection(
+            name=connection_name,
             source_id=source_id,
             destination_id=destination_id,
             api_root=self.api_root,
@@ -254,15 +245,6 @@ class CloudWorkspace:
             selected_stream_names=selected_streams,
             prefix=table_prefix or "",
         )
-
-        if isinstance(source, Source):
-            source._deployed_api_root = self.api_root  # noqa: SLF001
-            source._deployed_workspace_id = self.workspace_id  # noqa: SLF001
-            source._deployed_source_id = source_id  # noqa: SLF001
-        if cache:
-            cache._deployed_api_root = self.api_root  # noqa: SLF001
-            cache._deployed_workspace_id = self.workspace_id  # noqa: SLF001
-            cache._deployed_destination_id = deployed_connection.destination_id  # noqa: SLF001
 
         return CloudConnection(
             workspace=self,
@@ -285,7 +267,7 @@ class CloudWorkspace:
             connection_id=connection_id,
         )
 
-    def _permanently_delete_connection(
+    def permanently_delete_connection(
         self,
         connection: str | CloudConnection,
         *,
@@ -302,17 +284,17 @@ class CloudWorkspace:
                 connection_id=connection,
             )
 
-        delete_connection(
+        api_util.delete_connection(
             connection_id=connection.connection_id,
             api_root=self.api_root,
             api_key=self.api_key,
             workspace_id=self.workspace_id,
         )
         if delete_source:
-            self._permanently_delete_source(source=connection.source_id)
+            self.permanently_delete_source(source=connection.source_id)
 
         if delete_destination:
-            self._permanently_delete_destination(destination=connection.destination_id)
+            self.permanently_delete_destination(destination=connection.destination_id)
 
     # Run syncs
 
@@ -380,3 +362,76 @@ class CloudWorkspace:
         return connection.get_previous_sync_logs(
             limit=limit,
         )
+
+    # List sources, destinations, and connections
+
+    def list_connections(
+        self,
+        name: str | None = None,
+        *,
+        name_filter: Callable | None = None,
+    ) -> list[CloudConnection]:
+        """List connections by name in the workspace."""
+        connections = api_util.list_connections(
+            api_root=self.api_root,
+            api_key=self.api_key,
+            workspace_id=self.workspace_id,
+            name=name,
+            name_filter=name_filter,
+        )
+        return [
+            CloudConnection(
+                workspace=self,
+                connection_id=connection.connection_id,
+                source=None,
+                destination=None,
+            )
+            for connection in connections
+            if name is None or connection.name == name
+        ]
+
+    def list_sources(
+        self,
+        name: str | None = None,
+        *,
+        name_filter: Callable | None = None,
+    ) -> list[CloudSource]:
+        """List all sources in the workspace."""
+        sources = api_util.list_sources(
+            api_root=self.api_root,
+            api_key=self.api_key,
+            workspace_id=self.workspace_id,
+            name=name,
+            name_filter=name_filter,
+        )
+        return [
+            CloudSource(
+                workspace=self,
+                connector_id=source.source_id,
+            )
+            for source in sources
+            if name is None or source.name == name
+        ]
+
+    def list_destinations(
+        self,
+        name: str | None = None,
+        *,
+        name_filter: Callable | None = None,
+    ) -> list[CloudDestination]:
+        """List all destinations in the workspace."""
+        destinations = api_util.list_destinations(
+            api_root=self.api_root,
+            api_key=self.api_key,
+            workspace_id=self.workspace_id,
+            name=name,
+            name_filter=name_filter,
+        )
+        return [
+            CloudDestination(
+                workspace=self,
+                connector_id=destination.destination_id,
+            )
+            for destination in destinations
+            if name is None or destination.name == name
+        ]
