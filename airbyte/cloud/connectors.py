458e9fe7361723cfd0e0f7766b2b06c71d96e558
Aaron Steers <aj@airbyte.io>
2024-12-16 13:55:48 -0800

    add check action to cloud connectors

--- airbyte/cloud/connectors.py
+++ airbyte/cloud/connectors.py
@@ -6,9 +6,42 @@ from __future__ import annotations
 import abc
 from typing import TYPE_CHECKING, ClassVar, Literal
 
+from attr import dataclass
+
+from airbyte._util import api_util
+
 
 if TYPE_CHECKING:
     from airbyte.cloud.workspaces import CloudWorkspace
+
+
+@dataclass
+class CheckResult:
+    """A cloud check result object."""
+
+    success: bool
+    """Whether the check result is valid."""
+
+    error_message: str | None = None
+    """None if the check was successful. Otherwise the failure message from the check result."""
+
+    internal_error: str | None = None
+    """None if the check was able to be run. Otherwise, this will describe the internal failure."""
+
+    def __bool__(self) -> bool:
+        """Truthy when check was successful."""
+        return self.success
+
+    def __str__(self) -> str:
+        """Get a string representation of the check result."""
+        return "Success" if self.success else f"Failed: {self.error_message}"
+
+    def __repr__(self) -> str:
+        """Get a string representation of the check result."""
+        return (
+            f"CloudCheckResult(success={self.success}, "
+            f"error_message={self.error_message or self.internal_error})"
+        )
 
 
 class CloudConnector(abc.ABC):
@@ -42,6 +75,35 @@ class CloudConnector(abc.ABC):
             self.workspace.permanently_delete_source(self.connector_id)
         else:
             self.workspace.permanently_delete_destination(self.connector_id)
+
+    def check(
+        self,
+        *,
+        raise_on_error: bool = True,
+    ) -> CheckResult:
+        """Check the connector.
+
+        Returns:
+            A `CheckResult` object containing the result. The object is truthy if the check was
+            successful and falsy otherwise. The error message is available in the `error_message`
+            or by converting the object to a string.
+        """
+        result = api_util.check_connector(
+            workspace_id=self.workspace.workspace_id,
+            connector_type=self.connector_type,
+            actor_id=self.connector_id,
+            api_root=self.workspace.api_root,
+            client_id=self.workspace.client_id,
+            client_secret=self.workspace.client_secret,
+        )
+        check_result = CheckResult(
+            success=result[0],
+            error_message=result[1],
+        )
+        if raise_on_error and not check_result:
+            raise ValueError(f"Check failed: {check_result}")
+
+        return check_result
 
 
 class CloudSource(CloudConnector):
