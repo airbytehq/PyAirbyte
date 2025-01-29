a126d503dd7c2b74c896dcfd14dcef09430db2cc
Aaron Steers <aj@airbyte.io>
2025-01-28 21:59:14 -0800

    chore: remove pendulum dependencies

--- airbyte/logs.py
+++ airbyte/logs.py
@@ -15,13 +15,15 @@ import logging
 import os
 import platform
 import tempfile
+from time import strptime
 import warnings
 from functools import lru_cache
 from pathlib import Path
 
-import pendulum
 import structlog
 import ulid
+
+from airbyte_cdk.utils.datetime_helpers import ab_datetime_now, ab_datetime_format
 
 
 def _str_to_bool(value: str) -> bool:
@@ -60,6 +62,7 @@ def warn_once(
 
     if not with_stack:
         stacklevel = 0
+
     if with_stack is True:
         stacklevel = 2
 
@@ -139,7 +142,7 @@ def get_global_file_logger() -> logging.Logger | None:
     for handler in logger.handlers:
         logger.removeHandler(handler)
 
-    yyyy_mm_dd: str = pendulum.now().format("YYYY-MM-DD")
+    yyyy_mm_dd: str = ab_datetime_now().strftime("%Y-%m-%d")
     folder = AIRBYTE_LOGGING_ROOT / yyyy_mm_dd
     try:
         folder.mkdir(parents=True, exist_ok=True)
