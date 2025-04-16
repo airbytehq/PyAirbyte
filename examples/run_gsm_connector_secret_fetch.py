8257fbe20f539461bd9cd316e4b13bc9230bdda4
Aaron Steers <aj@airbyte.io>
2025-04-15 21:38:07 -0700

    update script

--- examples/run_gsm_connector_secret_fetch.py
+++ examples/run_gsm_connector_secret_fetch.py
@@ -10,7 +10,7 @@ If it doesn't exist, the script will fail. Users should ensure the directory
 exists and is excluded from git before running the script.
 
 Usage:
-    poetry run python examples/run_gsm_secret_fetch.py
+    poetry run python examples/run_gsm_connector_secret_fetch.py
 """
 
 from __future__ import annotations
@@ -21,7 +21,7 @@ import airbyte as ab
 from airbyte.secrets import GoogleGSMSecretManager, SecretHandle
 
 AIRBYTE_INTERNAL_GCP_PROJECT = "dataline-integration-testing"
-CONNECTOR_NAME = "source-s3"
+CONNECTOR_NAME = "source-klaviyo"
 
 AIRBYTE_REPO_ROOT = Path(__file__).parent.parent.parent / "airbyte"
 
@@ -33,7 +33,10 @@ CONNECTOR_SECRETS_DIR = (
     / CONNECTOR_NAME
     / "secrets"
 )
-
+if not AIRBYTE_REPO_ROOT.exists():
+    raise FileNotFoundError(f"Airbyte repo root does not exist: {AIRBYTE_REPO_ROOT}")
+if not CONNECTOR_SECRETS_DIR.exists():
+    CONNECTOR_SECRETS_DIR.mkdir(parents=True, exist_ok=True)
 
 def main() -> None:
     secret_mgr = GoogleGSMSecretManager(
@@ -42,12 +45,14 @@ def main() -> None:
     )
 
     secret: SecretHandle
-    for secret in secret_mgr.fetch_connector_secrets("source-s3"):
+    for secret in secret_mgr.fetch_connector_secrets(
+        connector_name=CONNECTOR_NAME,
+    ):
+        filename_base = "config"  # Default filename if not overridden
         if "filename" in secret.labels:
-            secret_file_path = (
-                CONNECTOR_SECRETS_DIR / f"{secret.labels['filename']}.json"
-            )
-            secret.write_to_file(secret_file_path)
+            filename_base = secret.labels["filename"]
+        secret_file_path = CONNECTOR_SECRETS_DIR / f"{filename_base}.json"
+        secret.write_to_file(secret_file_path)
 
 
 if __name__ == "__main__":
