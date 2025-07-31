99caa5a8dc2e479fc129c3c9f1c3a2bf13321f9c
Aaron ("AJ") Steers <aj@airbyte.io>
2025-07-30 21:30:36 -0700

    Delete tests/integration_tests/test_lowcode_connectors.py

--- tests/integration_tests/test_lowcode_connectors.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
-from __future__ import annotations
-
-from pathlib import Path
-
-
-UNIT_TEST_DB_PATH: Path = Path(".cache") / "unit_tests" / "test_db.duckdb"
