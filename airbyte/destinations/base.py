9d0db662d4c01d83009d9ad2d2f598ce915514ea
Jose Pefaur <jose.pefaur@gmail.com>
2025-08-14 09:58:36 -0500

    avoid null namespaces

--- airbyte/destinations/base.py
+++ airbyte/destinations/base.py
@@ -265,7 +265,7 @@ class Destination(ConnectorBase, AirbyteWriterInterface):
         with as_temp_files(
             files_contents=[
                 self._hydrated_config,
-                catalog_provider.configured_catalog.model_dump_json(),
+                catalog_provider.configured_catalog.model_dump_json(exclude_none=True),
             ]
         ) as [
             config_file,
